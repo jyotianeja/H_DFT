6d428161de2cc6a904766c84ef3bf5aed0716dbd
Paul Young <yyang173@illinois.edu>
2015-03-10 20:11:21 -0500

    fixed init

--- init.py
+++ init.py
@@ -3,9 +3,9 @@ import numpy as np
 def LegalKVecs(maxK,L=1.0):
     kList=[]
     # calculate a list of legal k vectors
-    for kx in range(maxK):
-        for ky in range(maxK):
-            for kz in range(maxK):
+    for kx in range(-maxK,maxK+1):
+        for ky in range(-maxK,maxK+1):
+            for kz in range(-maxK,maxK+1):
                 kList.append([2*np.pi/L*kx,2*np.pi/L*ky,2*np.pi/L*kz])
             # end kz
         # end ky
@@ -13,6 +13,20 @@ def LegalKVecs(maxK,L=1.0):
     return kList
 # end def 
 
+<<<<<<< HEAD
+def Kmag(kList):
+    magList = []
+    for k in kList:
+        magList.append( np.linalg.norm(k) )
+    # end for k
+    return magList
+# end def kMag
+
+if __name__=="__main__":
+    print LegalKVecs(1)
+    print Kmag(LegalKVecs(1))
+# end __main__
+=======
 
 def kMag(kList):
     magList = []
@@ -21,3 +35,4 @@ def kMag(kList):
     # end for k
     return magList
 # end def kMag
+>>>>>>> 5ccfde210cb6c57689bf438afe48b4009d6b4e96
