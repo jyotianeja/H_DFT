dadbe2dcbbe58d9587d632bfa2799465a3424a97
Paul Young <yyang173@illinois.edu>
2015-03-10 20:45:50 -0500

    added density related stuff

--- init.py
+++ init.py
@@ -13,7 +13,6 @@ def LegalKVecs(maxK,L=1.0):
     return kList
 # end def 
 
-<<<<<<< HEAD
 def Kmag(kList):
     magList = []
     for k in kList:
@@ -22,17 +21,33 @@ def Kmag(kList):
     return magList
 # end def kMag
 
-if __name__=="__main__":
-    print LegalKVecs(1)
-    print Kmag(LegalKVecs(1))
-# end __main__
-=======
+def normalizeDensity(n,N=1):
+    # normalize density n to have the number of electrons N
+    norm = np.linalg.norm(n)
+    for i in range(len(n)):
+        n[i] *= N/norm
+    # end for
+# end def normalizeDensity
 
-def kMag(kList):
-    magList = []
-    for k in kList:
-        magList.append( np.array(k).norm() )
-    # end for k
-    return magList
-# end def kMag
->>>>>>> 5ccfde210cb6c57689bf438afe48b4009d6b4e96
+def newDensity(w,v,n=1):
+    newn = []
+    for i in range(n):
+        newn = epsilon[i]*np.array(psi[i])*np.array(psi[i])
+    # end for i
+    return newn
+# end def newDensity
+
+def mixDensity(newn,oldn,beta=0.1):
+    return beta*np.array(newn)+np.array(oldn)
+# end def mixDensity
+
+if __name__=="__main__":
+    #print LegalKVecs(1)
+    #print Kmag(LegalKVecs(1))
+    psi = [[1,0,0],[0,0,1]]
+    epsilon = [1,2]
+    newn = newDensity(psi,epsilon)
+    oldn = [0,.5,0]
+    normalizeDensity(oldn)
+    print oldn
+# end __main__
