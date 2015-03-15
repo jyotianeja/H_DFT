d9ab86d0a6646ebc77c9cea6820eeac73e78c235
Paul Young <yyang173@illinois.edu>
2015-03-15 16:32:24 -0500

    attemping to fix density constraints
    Density must be:
    1. real
    2. normalized
    
    psi^* psi is also tricky in k space

--- mini_project_hydrogen_main.py
+++ mini_project_hydrogen_main.py
@@ -1,27 +1,25 @@
-
-
-import numpy, math, random, cmath
-import matplotlib.pyplot as pl
+import numpy, random
 
 from init import *
 
 
 PI = np.pi
 maxK = 3
+L=5.0
 maxNsteps = 100
 
-thres = 1e-2
+thres = 1e-3
 l = 1+2*maxK
-oldn = np.random.random(l*l*l)
-normalizeDensity(oldn)
+#oldn = np.random.random(l*l*l)
+#normalizeDensity(oldn)
 
-def Hamiiltonian(KList,n_k,):
+def Hamiiltonian(KList,n_k):
 	mag = Kmag(KList)
 	n = len(KList)
 	H = np.zeros((n,n))
 	for i in range (0,n):
 		if mag[i] !=0:
-			H[i][i] = -0.5*pow(mag[i],2) + 8*PI*PI/pow(mag[i],2) + 8*PI*PI*n_k[i]/pow(mag[i],2) 
+			H[i][i] = -0.5*pow(mag[i],2)*(L**3) - 8*PI*PI/pow(mag[i],2)*(L**3) + 8*PI*PI*n_k[i]/pow(mag[i],2)*(L**3)
 		# end if
     # end for
 	return H	
@@ -29,12 +27,19 @@ def Hamiiltonian(KList,n_k,):
 
 if __name__ == "__main__":
 
+    KList = LegalKVecs(maxK,L)
+    oldn = guessDensity(KList)
+    normalizeDensity(oldn,KList,L)
+    
+    plotDensity(oldn,KList)
+    
     converged = False
     for step in range(maxNsteps):
-        w,v = np.linalg.eig(Hamiiltonian(LegalKVecs(maxK),oldn))
-        newn = newDensity(w,v)
-        newn = mixDensity(newn,oldn,0.2)
-        normalizeDensity(newn)
+        w,v = np.linalg.eig(Hamiiltonian(KList,oldn))
+        newn = newDensity(w,v,KList)
+        newn = mixDensity(newn,oldn,.1)
+        normalizeDensity(newn,KList,L)
+
         if np.linalg.norm(newn-oldn)<thres:
             converged = True
             break
@@ -42,15 +47,16 @@ if __name__ == "__main__":
         if not (step%10):
             print np.linalg.norm(newn-oldn)
         # end if
-        
+          
         oldn = newn  
     # end for 
 	
     if converged:
-        print "converged in ", step, " steps, energy=", w.min()
+        print "converged in ", step, " steps, energy=", min(w)
     else:
-        print "Did not converge in ",step, " steps, energy=",w.min()
+        print "Did not converge in ",step, " steps, energy=", min(w)
     # end if
+    plotDensity(newn,KList)
     
 # end __main__
 
