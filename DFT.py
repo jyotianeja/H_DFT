3856cb8f7231ee14ae00d4ba11e2e255907d7d49
Paul Young <yyang173@illinois.edu>
2015-04-22 17:42:40 -0500

    Fixed psi2density
    Converges, but only five -0.5 for certain basis separation

--- DFT.py
+++ DFT.py
@@ -4,21 +4,47 @@ import random
 import numpy as np
 
 N = 1
-maxK = 5
+maxK = 2
+basisSep=2.13
 maxNsteps = 500
 thres = 1e-5
-mixing = 0.05
+mixing = 0.5
 
 def normalize(wavevec):
     # assume each planewave basis function is normalize
     # normalize wf such that density integrates to 1
     norm = np.linalg.norm(wavevec)
-    wavevec=wavevec/norm
+    for i in range(len(wavevec)):
+        wavevec[i] /= norm
+    # end for i
 # end def normalize
 
-kList=[[kx,ky,kz] for kx in range(-maxK,maxK+1) for ky in range(-maxK,maxK+1) for kz in range(-maxK,maxK+1) if not (kx==ky==kz==0)]
+# basis for wavevec
+kList=[[basisSep*kx,basisSep*ky,basisSep*kz] for kx in range(-maxK,maxK+1) for ky in range(-maxK,maxK+1) for kz in range(-maxK,maxK+1) if not (kx==ky==kz==0)]
 kMag = [np.linalg.norm(k) for k in kList]
 kSize=len(kList)
+
+def psi2density(wavevec):
+    density=np.zeros(kSize)
+    for i in range(len(wavevec)):
+        ci=wavevec[i]
+        ki = kList[i]
+        for j in range(len(wavevec)):
+            cj=wavevec[j]
+            kj=kList[j]
+            
+            idx = 0
+            kdiff = list( np.array(ki)-np.array(kj) )
+            for k in range(len(kList)):
+                if kdiff==kList[k]:
+                    idx = k
+                # end if kdiff
+            # end for k
+            density[idx] += ci*cj 
+        # end for j
+    # end for i
+    return density
+# end def psi2density
 
 def HamVec(n_k):
     # return Hamiltonian on planewave basis grid
@@ -29,18 +55,20 @@ def HamVec(n_k):
     for i in range(0,kSize)]
 # end def HamVec
 
+
+import matplotlib.pyplot as plt
 if __name__ == "__main__":
 
     # make an initial guess
     psi = np.random.random(pow(1+2*maxK,3) -1)
     normalize(psi)
-
     # self-consistently solve the KS equations
     energy=0
     converged = False
+    
     for step in range(maxNsteps):
-        print(energy)
-        H=HamVec(psi*psi) # !!!! This is not correct !!!!
+        print energy
+        H=HamVec(psi2density(psi)) # !!!! This is not correct !!!!
         newPsi=np.zeros(kSize)
         newPsi[H.index(min(H))]=1
         psi =psi*(1-mixing)+newPsi*mixing
@@ -52,13 +80,24 @@ if __name__ == "__main__":
         # end if thres
         energy = newEnergy
     # end for step
-
+    
     if converged:
         print "converged in ", step, " steps, energy=", energy
-        plt.plot(psi)
+        
+        x = []
+        print psi
+        y = np.ones( 9 )
+        for i in range( len(kMag) ):
+            kmag = kMag[i]
+            if kmag not in x:
+                x.append(kmag)
+            # end if 
+            y[x.index(kmag)] += psi[i]
+        # end for i
+        plt.plot(x,y,'-x')
         plt.show()
     else:
         print "Did not converge in ",step, " steps, energy=",energy
     # end if converged
-        
+   
 # end __main__
